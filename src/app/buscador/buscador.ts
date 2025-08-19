0bcdeb62d65a5e8dffbdaa65af218ea5923c8fde
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-19 00:46:51 -0300

    Update buscador.ts

--- src/app/buscador/buscador.ts
+++ src/app/buscador/buscador.ts
@@ -1,11 +1,32 @@
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { Usuario } from '../usuario';
+import { UsuarioModelo } from '../modelo';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-buscador',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './buscador.html',
-  styleUrl: './buscador.css'
+  styleUrls: ['./buscador.css']
 })
 export class Buscador {
+  form: FormGroup;
+  resultados: UsuarioModelo[] = [];
 
+  constructor(private fb: FormBuilder, private usuario: Usuario) {
+    this.form = this.fb.group({
+      nombre: ['']
+    });
+  }
+
+  buscar() {
+    const nombre = this.form.get('nombre')?.value;
+    this.usuario.buscar(nombre).subscribe((data: UsuarioModelo[]) => {
+      this.resultados = data;
+    });
+  }
 }
